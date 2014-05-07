7e3c9e2d0b0f438e853abbe31861a23fc692383d
Randy <rair@rair-VirtualBox.(none)>
2014-05-07 00:27:28 -0400

    needs debugging

--- Cache.h
+++ Cache.h
@@ -49,33 +49,54 @@ private:
     MAPPING_TYPE mapping;
     WRITE_POLICY writeP;
     QList<INSTRUCTION_TYPE> RWList;
-    QList<int> addressList;
+    QList<long> addressList;
 
-    QList< QVector<int>* >* cacheData;
+    QList< QVector<long int>* >* cacheData;
     QList< QVector<int>* >* ageData;    //keep track of how recently used a block is; smaller the number, the older the data
+    QList< QVector<bool>* >* dirtyBits; //keeps track of dirty bits for each block
 
 
     int blockNum;       //keeps track of how many blocks total there are
-    int waySize;        //keeps track of how many blocks are in a way
+    int waySize;        //keeps track of how many blocks are in a way; also the number of sets there are
 
-    int dirtyBits;      //keeps track of how many dirty bits there are
+
     int hitNum;         //keeps track of how many hits there are
     int M2C;            //keeps track of how many bytes of transfer from memory to cache there are
     int C2M;            //keeps track of how many bytes of transfer from cache to memory there are
     int compNum;        //keeps track of number of ways
     int age;            //keeps track of how new the current instruction is
+    int instructionNum;
 
     //private functions
-    bool findTagAt(int tagToFind, int setToCheck);
-    bool findTag();
+    int findTag(long int tagToFind, int setToCheck);
     void init();        //initializes the cacheData QList
     bool isNull(int wayToCheck, int setToCheck);
-    int findOldest(int setToCheck);
-    int at(int wayToCheck, int setToCheck);
-    void replace(int wayToCheck, int setToCheck, int newVal);
+    int findEmptyWay(int setToCheck);
+
+    //Access functions
+    int findOldest(int setToCheck) const;
+    long at(int wayToCheck, int setToCheck) const;
+    bool DBAt(int wayToCheck, int setToCheck) const;
+
+
+
+    //modifier functions
+    void replace(int wayToCheck, int setToCheck, long int newVal);
+    void setAge(int wayToCheck, int setToCheck, int newAge);
+    void setDB(int wayToCheck, int setToCheck, bool newDB);
+
+
+
 
 public:
     Cache(CACHE_SIZE newCache = c_1K, BLOCK_SIZE newBlock = B_8b, MAPPING_TYPE newMap = DM, WRITE_POLICY newWrite = WB);
+
+    //Access functions
+    int getM2C() const;
+    int getC2M() const;
+    int getCompNum() const;
+    int getHits() const;
+    int getInstructionNum() const;
 
     void readFile(QString fileName);
     void iterateInput();
@@ -87,8 +108,8 @@ public:
 
     CACHE_SIZE getCacheSize();
     BLOCK_SIZE getBlockSize();
-    MAPPING_TYPE getMappingType();
-    WRITE_POLICY getWritePolicy();
+    QString getMappingType();
+    QString getWritePolicy();
 
 
     void simulate();
@@ -97,25 +118,23 @@ public:
 ///////////////////////////////
 //PRIVATE HELPER FUNCTIONS
 //////////////////////////////
-//iterates through the ways and checks a par
-bool Cache::findTagAt(int tagToFind, int setToCheck)
+//iterates through the ways and checks a particular set to see if a tag matches
+//if it does, it returns the way's index, otherwise it returns -1
+int Cache::findTag(long tagToFind, int setToCheck)
 {
     for(int i = 0; i < cacheData->size(); i++)
     {
-        if (cacheData->at(i)->at(setToCheck) == tagToFind) return true; //
+        if (cacheData->at(i)->at(setToCheck) == tagToFind) return i; //
     }
-    return false;
-}
-
-bool Cache::findTag()
-{
+    return -1;
 }
 
 void Cache::init()
 {
-    cacheData = new QList< QVector<int>* >; //Create the database of QVectors (ways) that will store tags
-    blockNum = cacheS/blockS;
-
+    cacheData = new QList< QVector<long>* >; //Create the database of QVectors (ways) that will store tags
+    ageData = new QList< QVector<int>* >;    //Create the database of ages for each block in the cacheData
+    dirtyBits = new QList< QVector<bool>* >; //create the database of dirtybits for each block in cacheData
+    this->blockNum = cacheS/blockS;
     switch(mapping){
     case DM:
         compNum = 1;
@@ -134,16 +153,45 @@ void Cache::init()
     }
 
 
-    waySize = blockNum/compNum;
+    this->waySize = blockNum/compNum;
+
+    //FDB
+    qDebug() << "done setting waySize to " << waySize << " and setting compNum to " << compNum;
+
     for(int i = 0; i < compNum; i++)
     {
-        QVector<int>* temp = new QVector<int>(waySize);
+        //FDB
+        qDebug() << "started vector " << i;
+        QVector<long>* temp = new QVector<long>(waySize);
+        temp->fill(NULL);
         cacheData->append(temp);
 
+        //FDB
+        qDebug() << "added way vector" << i;
+
         QVector<int>* ageTemp = new QVector<int>(waySize);
+        //FDB
+        qDebug() << "constructed age vector" << i;
         ageTemp->fill(0);
-        cacheData->append(temp);
+        qDebug() << "filled age vector" << i;
+        ageData->append(ageTemp);
+
+        //FDB
+        qDebug() << "added age vector" << i;
+
+        QVector<bool>* dirtyTemp = new QVector<bool>(waySize);
+        dirtyTemp->fill(false);         //initialize everything as false
+        dirtyBits->append(dirtyTemp);
+
+        //FDB
+        qDebug() << "added dirty vector" << i;
+
+        //FDB
+        qDebug() << "finished vector " << i;
+
     }
+    //FDB
+    qDebug() << "done preparing vectors";
 }
 
 bool Cache::isNull(int wayToCheck, int setToCheck)
@@ -152,7 +200,23 @@ bool Cache::isNull(int wayToCheck, int setToCheck)
     else return false;
 }
 
-int Cache::findOldest(int setToCheck)
+//Finds an empty way given a set and returns that way's index
+//Otherwise returns -1
+int Cache::findEmptyWay(int setToCheck)
+{
+    for(int wayInd = 0; wayInd < cacheData->size(); wayInd++)
+    {
+        if(this->isNull(wayInd,setToCheck)) //Check if this is empty
+        {
+            return wayInd;
+
+        }
+    }
+    return -1;
+}
+
+//Returns the oldest way in a set
+int Cache::findOldest(int setToCheck) const
 {
     int oldestIndex = 0;
     for(int i = 0; i < compNum; i++)
@@ -163,14 +227,34 @@ int Cache::findOldest(int setToCheck)
     return oldestIndex;
 }
 
-int Cache::at(int wayToCheck, int setToCheck)
+long Cache::at(int wayToCheck, int setToCheck) const
 {
     return cacheData->at(wayToCheck)->at(setToCheck);
 }
 
-void Cache::replace(int wayToCheck, int setToCheck, int newVal)
+bool Cache::DBAt(int wayToCheck, int setToCheck) const
+{
+    return dirtyBits->at(wayToCheck)->at(setToCheck);
+
+}
+
+
+
+
+
+void Cache::replace(int wayToCheck, int setToCheck, long newVal)
 {
     cacheData->at(wayToCheck)->replace( setToCheck, newVal);
+}
+
+void Cache::setAge(int wayToCheck, int setToCheck, int newAge)
+{
+    ageData->at(wayToCheck)->replace( setToCheck, newAge);
+}
+
+void Cache::setDB(int wayToCheck, int setToCheck, bool newDB)
+{
+    dirtyBits->at(wayToCheck)->replace( setToCheck, newDB);
 }
 
 ///////////////////////////////
@@ -182,6 +266,28 @@ Cache::Cache(CACHE_SIZE newCache, BLOCK_SIZE newBlock, MAPPING_TYPE newMap, WRIT
     this->blockS = newBlock;
     this->mapping = newMap;
     this->writeP = newWrite;
+
+}
+
+int Cache::getM2C() const
+{
+    return M2C;
+}
+int Cache::getC2M() const
+{
+    return C2M;
+}
+int Cache::getCompNum() const
+{
+    return compNum;
+}
+int Cache::getHits() const
+{
+    return hitNum;
+}
+int Cache::getInstructionNum() const
+{
+    return instructionNum;
 }
 
 void Cache::readFile(QString fileName)
@@ -206,12 +312,16 @@ void Cache::readFile(QString fileName)
     {
         QString readLine = stream.readLine();
         QStringList lineData = readLine.split("\t");
-        if(lineData.at(0) == "read")
+        if(lineData.at(0).contains("re"))
             RWList.append(read_I);
-        else RWList.append(write_I);
-        int tempAddress = lineData.at(1).trimmed().remove(0,2).toInt(0,16); //remove the newline character and convert to int
+        else if(lineData.at(0).contains("wr")) RWList.append(write_I);
+        long tempAddress = lineData.at(1).trimmed().remove(0,2).toLong(0,16); //remove the newline character and convert to int
         addressList.append(tempAddress);
     }
+    instructionNum = RWList.size();
+
+    //FDB
+    qDebug() <<"There are " << instructionNum << " instructions.";
 
 }
 
@@ -220,7 +330,16 @@ void Cache::iterateInput()
 {
     for(int i = 0; i < addressList.size(); i++)
     {
-        qDebug() << RWList.at(i) << "\t" << addressList.at(i);
+        QString insType;
+        if(RWList.at(i) == read_I)
+        {
+            insType = "read";
+        }
+        else if(RWList.at(i) == write_I)
+        {
+            insType = "write";
+        }
+        qDebug() << insType << "\t" << addressList.at(i);
     }
 }
 
@@ -255,51 +374,182 @@ BLOCK_SIZE Cache::getBlockSize()
     return blockS;
 }
 
-MAPPING_TYPE Cache::getMappingType()
+QString Cache::getMappingType()
 {
-    return mapping;
+    QString cacheType;
+    switch(mapping){
+    case DM:
+        cacheType = "DM";
+        break;
+    case two_way:
+        cacheType = "2W";
+        break;
+    case four_way:
+        cacheType = "4W";
+        break;
+    case FA:
+        cacheType = "FA";
+        break;
+    default:
+        cacheType = "none";
+    }
+    return cacheType;
 }
 
-WRITE_POLICY Cache::getWritePolicy()
+QString Cache::getWritePolicy()
 {
-    return writeP;
+    QString writeType;
+    if(writeP == WB) writeType = "WB";
+    else writeType = "WT";
+    return writeType;
 }
 
 
 
-
+//Does a simulation on a set of instructions
 void Cache::simulate()
 {
     this->init();
-    dirtyBits = 0;      //keeps track of how many dirty bits there are
+    //FDB
+    qDebug() << "init done";
     hitNum = 0;         //keeps track of how many hits there are
     M2C = 0;            //keeps track of how many bytes of transfer from memory to cache there are
     C2M = 0;            //keeps track of how many bytes of transfer from cache to memory there are
     age = 0;            //keeps track of when instruction was used
+    //FDB
 
-    for(int i = 0; i < RWList.size(); i++)  //iterate through the instructions
+    for(int insInd = 0; insInd < RWList.size(); insInd++)  //iterate through the instructions
     {
-        int currentAddress = addressList.at(i);
-        int currentTag = (int)(currentAddress/compNum);
-        //look for a place to put tag
-        for(int wayInd = 0; wayInd < cacheData->size(); wayInd++)
+        long currentAddress = addressList.at(insInd);
+        long currentTag = (long)(currentAddress/compNum);
+        //FDB
+        qDebug() << "The tag is" << currentTag;
+        int currentSet = (int)((currentAddress/blockS)%waySize);
+        //FDB
+        qDebug() << "Belongs in set" << currentSet;
+        int currentWay;     //goal is to find this
+        ////////////////////////////////////////////////////////////////////\\
+        //ON A READ  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+        if(RWList.at(insInd) == read_I)
         {
-            bool placementFound = false;
-            //iterate through the set
-            for(int setInd = 0; setInd < cacheData->at(wayInd)->size(); setInd++)
+            currentWay = findTag(currentTag, currentSet);
+            //////////////////////////////////////////////////////
+            //ON A HIT
+            if(currentWay != -1)   //if a hit
             {
-                if(this->isNull(wayInd,setInd))
+                //FDB
+                qDebug() << "read hit" << currentWay;
+                hitNum++;
+            }
+            /////////////////////////////////////////////////////
+            //ON A MISS
+            else
+            {
+                //look for a place to put tag
+                ////////////////////
+                //EMPTY BLOCK FOUND
+                if((currentWay = this->findEmptyWay(currentSet)) != -1)
                 {
-                    this->replace(wayInd,setInd,currentTag);
-                    placementFound = true;
+                    this->replace(currentWay,currentSet,currentTag);    //if so put it in there
+                }
+                //////////////////
+                //NO EMPTY BLOCKS
+                else
+                {
+                    currentWay = findOldest(currentSet);                    //Find the least recently used
+                    this->replace(currentWay, currentSet, currentTag);      //replace the least recently used
+                    if(this->DBAt(currentWay,currentSet))                   //if there is a dirty bit
+                    {
+                        C2M += blockS;                                      //cache to memory transfer due to writeback
+                        this->setDB(currentWay,currentSet,false);           //set dirty bit to false
+                    }
+
+                }
+                M2C += blockS;  //memory to cache transfer
+            }
+
+        }
+        ////////////////////////////////////////////////////////////////////\\
+        //ON A WRITE  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+        else if(RWList.at(insInd) == write_I)
+        {
+            /////////////////////////////////////////////////
+            //ON A WRITEBACK
+            if(writeP == WB)
+            {
+                currentWay = findTag(currentTag, currentSet);
+                ///////////////////////////////////////////
+                //ON A HIT
+                if(currentWay != -1)   //if a hit
+                {
+                    hitNum++;
+                    setDB(currentWay, currentSet, true);
+                }
+                //////////////////////////////////
+                //ON A MISS
+                else
+                {
+                    /////////////////////
+                    //EMPTY BLOCK FOUND
+                    if((currentWay = this->findEmptyWay(currentSet)) != -1)
+                    {
+                        this->replace(currentWay,currentSet,currentTag);    //if so put it in there
+                    }
+                    //////////////////
+                    //NO EMPTY BLOCKS
+                    else
+                    {
+                        currentWay = findOldest(currentSet);                    //Find the least recently used
+                        this->replace(currentWay, currentSet, currentTag);      //replace the least recently used
+                        if(this->DBAt(currentWay,currentSet))                   //if there is a dirty bit
+                        {
+                            C2M += blockS;                                          //cache to memory transfer due to writeback
+                        }
+                        this->setDB(currentWay,currentSet,true);                //set dirty bit to true
+
+                    }
+                    M2C += blockS;  //memory to cache transfer
                 }
             }
-            if(!placementFound)
+            else if(writeP == WT)
             {
+                currentWay = findTag(currentTag, currentSet);
+                ////////////////////////////////////////
+                //ON A HIT
+                if(currentWay != -1)
+                {
+                    //FDB
+                    qDebug() << "Write hit" << currentWay;
+                    hitNum++;
+                }
+                ////////////////////////////////////////
+                //ON A MISS
+                else
+                {
+                    //////////////////////
+                    //EMPTY BLOCK FOUND
+                    if((currentWay = this->findEmptyWay(currentSet)) != -1)
+                    {
+                        this->replace(currentWay,currentSet,currentTag);        //put tag in empty block
+                    }
+                    //////////////////////
+                    //NO EMPTY BLOCKS
+                    else
+                    {
+                        currentWay = findOldest(currentSet);                    //Find the least recently used
+                        this->replace(currentWay, currentSet, currentTag);      //replace the least recently used
+                    }
 
+                    M2C += blockS;    //memory to cache transfer bceause miss
+                }
+                C2M += 4;      //cache to memory tranfer due to immediate write from writethrough
             }
+
         }
+        this->setAge(currentWay,currentSet,age);    //set the age
+        age++;                                      //increment age
     }
+
 }
 
 #endif // CACHE_H
--- main.cpp
+++ main.cpp
@@ -20,10 +20,27 @@ enum test2
 
 int main()
 {
-    int currentAddress = 70;
-    int compNum = 37;
-    int currentTag = (int)(currentAddress/compNum);
-    qDebug() << currentTag;
+    Cache* C1 = new Cache(c_1K, B_8b, four_way, WT);
+    qDebug() << "cache constructed";
+    C1->readFile("test");
+    qDebug() << "read done";
+
+
+    C1->simulate();
+    qDebug() << C1->getCacheSize() << "\t"
+             << C1->getBlockSize() << "\t"
+             << C1->getMappingType() << "\t"
+             << C1->getWritePolicy() << "\t"
+             << QString::number((C1->getHits()/C1->getInstructionNum()),'f',2) << "\t"
+             << C1->getM2C() << "\t"
+             << C1->getC2M() << "\t"
+                << C1->getCompNum();
+    qDebug() << "There are " << C1->getInstructionNum() << " instructions and " << C1->getHits() << " hits.";
+
+
+    //C1->iterateInput();
+
+    delete C1;
     /*
     Cache* C1 = new Cache();
     C1->readFile("test");
